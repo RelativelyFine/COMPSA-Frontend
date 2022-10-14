b5f7eac8e7e61d4822a83e139d8274647859ea9e
KnlnKS <soulkks@hotmail.ca>
2022-10-14 19:05:25 -0400

    🎨 Create circuit sub menu component

--- components/Navbar/MobileNav/MobileNav.jsx
+++ components/Navbar/MobileNav/MobileNav.jsx
@@ -1,20 +1,17 @@
-import React, { Fragment } from "react";
+import React from "react";
 import Image from "next/image";
-import { Menu, Transition } from "@headlessui/react";
-import { ChevronDownIcon } from "@heroicons/react/solid";
-import { SiDiscord } from "react-icons/si";
-
-import navItems from "../navItems";
-import MobileNavItem from "./MobileNavItem";
-import SocialMediaItem from "./SocialMediaItem";
-
 import {
   AiOutlineMenuFold,
   AiFillLinkedin,
   AiFillInstagram,
   AiFillFacebook,
 } from "react-icons/ai";
-import CircuitSubMenuItems from "../CircuitSubMenuItems";
+import { SiDiscord } from "react-icons/si";
+
+import navItems from "../navItems";
+import MobileNavItem from "./MobileNavItem";
+import SocialMediaItem from "./SocialMediaItem";
+import CircuitSubMenu from "../CircuitSubMenu";
 
 const MobileNav = ({ handleNav, nav }) => (
   <div
@@ -53,32 +50,7 @@ const MobileNav = ({ handleNav, nav }) => (
       <div className="py-4 flex flex-col">
         <ul>
           <li>
-            <div>
-              <Menu as="div" className="relative inline-block text-left ">
-                <div className="">
-                  <Menu.Button className="inline-flex w-full justify-center rounded-md px-4 py-5 text-xl font-medium hover:bg-opacity-30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 text-black hover:text-yellow-500">
-                    CIRCUIT
-                    <ChevronDownIcon
-                      className="ml-2 -mr-1 h-5 w-5 text-black"
-                      aria-hidden="true"
-                    />
-                  </Menu.Button>
-                </div>
-                <Transition
-                  as={Fragment}
-                  enter="transition ease-out duration-100"
-                  enterFrom="transform opacity-0 scale-95"
-                  enterTo="transform opacity-100 scale-100"
-                  leave="transition ease-in duration-75"
-                  leaveFrom="transform opacity-100 scale-100"
-                  leaveTo="transform opacity-0 scale-95"
-                >
-                  <Menu.Items className="absolute left-0 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none z-50">
-                    <CircuitSubMenuItems />
-                  </Menu.Items>
-                </Transition>
-              </Menu>
-            </div>
+            <CircuitSubMenu isMobile={true} />
           </li>
           {navItems.map((item) => (
             <MobileNavItem key={item.text} {...item} />
--- components/Navbar/Navbar.jsx
+++ components/Navbar/Navbar.jsx
@@ -1,13 +1,11 @@
-import React, { useState, Fragment } from "react";
-import { Menu, Transition } from "@headlessui/react";
+import React, { useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
-import { ChevronDownIcon } from "@heroicons/react/solid";
 import { AiOutlineMenuUnfold } from "react-icons/ai";
 
 import navItems from "./navItems";
 import MobileNav from "./MobileNav";
-import CircuitSubMenuItems from "./CircuitSubMenuItems";
+import CircuitSubMenu from "./CircuitSubMenu";
 
 const colortheme = (color, type) => {
   if (color === "light") {
@@ -64,32 +62,7 @@ const Navbar = (props) => {
               </Link>
             ))}
             <li className="ml-10 text-lg hover:border-b font-bold transition transform hover:-translate-y-[2px] motion-reduce:transition-none motion-reduce:hover:transform-none bg-white p-1 px-2 rounded-xl">
-              <div className="">
-                <Menu as="div" className="relative inline-block text-left ">
-                  <div className="">
-                    <Menu.Button className="inline-flex w-full justify-center rounded-md px-4 py-2 text-sm font-medium hover:bg-opacity-30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 text-black hover:text-yellow-500">
-                      CIRCUIT
-                      <ChevronDownIcon
-                        className="ml-2 -mr-1 h-5 w-5 text-black"
-                        aria-hidden="true"
-                      />
-                    </Menu.Button>
-                  </div>
-                  <Transition
-                    as={Fragment}
-                    enter="transition ease-out duration-100"
-                    enterFrom="transform opacity-0 scale-95"
-                    enterTo="transform opacity-100 scale-100"
-                    leave="transition ease-in duration-75"
-                    leaveFrom="transform opacity-100 scale-100"
-                    leaveTo="transform opacity-0 scale-95"
-                  >
-                    <Menu.Items className="absolute right-0 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-                      <CircuitSubMenuItems />
-                    </Menu.Items>
-                  </Transition>
-                </Menu>
-              </div>
+              <CircuitSubMenu isMobile={false} />
             </li>
           </ul>
           <div
