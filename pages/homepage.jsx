ab3d5e7dfc79f91b231022c03527a978646a2285
KnlnKS <soulkks@hotmail.ca>
2022-10-19 01:06:50 -0400

    🔀 Merge from main

--- pages/homepage.jsx
+++ pages/homepage.jsx
@@ -1,4 +1,4 @@
-import Head from "next/head";
+import Header from "../components/Header";
 import Navbar from "../components/Navbar";
 import Main from "../components/Main";
 import Footer from "../components/Footer";
@@ -6,10 +6,7 @@ import Footer from "../components/Footer";
 export default function Home() {
   return (
     <div>
-      <Head>
-        <title>Queen&lsquo;s Compsa</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      </Head>
+      <Header/>
       <Main />
       <Navbar color="dark" />
       <Footer />
