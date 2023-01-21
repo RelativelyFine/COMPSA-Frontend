5853709d208ff2e509f76f348d96a68c1efee1a1
Adwait Srivastava <studyuse1702@gmail.com>
2023-01-21 00:45:03 -0500

    Creation of pd page

---
+++ components/Prof_Dev/P_d.jsx
@@ -0,0 +1,144 @@
+import React from 'react'
+import Link from 'next/link'
+
+const Prof_dev = ()=> {
+    return(
+      <div className='mb-24'>
+    <div className='flex items-center w-full flex-col bg-[#F5F5F5]'>
+    <h1 className='font-inter font-bold tracking-wider text-[#553ADD] text-center text-xl sm:text-2xl md:text-3xl pb-6 pt-24'>PROFESSIONAL DEVELOPMENT</h1>
+    <h1 className='font-inter font-bold tracking-wider text-[#200263] text-center text-xl sm:text-3xl md:text-5xl pb-6'>Upcoming Events</h1>
+      <div className='grid grid-cols-5 divide-x bg-[#553ADD] rounded-3xl w-3/5 h-[18vw] pl-2 sm:pl-8 '>
+        <div className='flex-col text-[#EEEBFD] font-inter font-bold sm:col-span-1'>
+          <h1 className='flex text-xl sm:text-3xl md:text-6xl pt-3 md:pt-9 overflow-hidden'>9</h1>
+          <h2 className='flex text-xs sm:text-md md:text-xl overflow-hidden'>September</h2>
+          <h3 className='flex text-xs sm:text-sm md:text-lg overflow-hidden'>5:00PM</h3>
+
+          
+          </div>
+
+          <div className='grid col-span-4 text-[#EEEBFD] font-inter font-normal '>
+          <h1 className='flex-no-wrap text-md sm:text-3xl md:text-5xl pt-3 pl-5 md:pt-9 md:pl-6'>Rate my resume</h1>
+          <div className='grid justify-items-end mr-10 mt-6'>
+          <Link href="/" className='flex place-items-center'>
+            <button className='flex-text-center text-[#200263] font-bold text-xs sm:text-md md:text-xl bg-[#FEFEFE] rounded-xl w-1/5 h-1/2'>
+            RSVP
+            </button>
+           
+          </Link> 
+          
+          
+          </div>
+          </div>
+          
+
+         
+
+      </div>
+      <div className='grid grid-cols-5 divide-x bg-[#553ADD] rounded-3xl w-3/5 h-[18vw] pl-2 sm:pl-8 mt-10 '>
+        <div className='flex-col text-[#EEEBFD] font-inter font-bold sm:col-span-1'>
+          <h1 className='flex text-xl sm:text-3xl md:text-6xl pt-3 md:pt-9 overflow-hidden'>9</h1>
+          <h2 className='flex text-xs sm:text-md md:text-xl overflow-hidden'>September</h2>
+          <h3 className='flex text-xs sm:text-sm md:text-lg overflow-hidden'>5:00PM</h3>
+
+          
+          </div>
+
+          <div className='grid col-span-4 text-[#EEEBFD] font-inter font-normal '>
+          <h1 className='flex-no-wrap text-md sm:text-3xl md:text-5xl pt-3 pl-5 md:pt-9 md:pl-6'>Rate my resume</h1>
+          <div className='grid justify-items-end mr-10 mt-6'>
+          <Link href="/" className='flex place-items-center'>
+            <button className='flex-text-center text-[#200263] font-bold text-xs sm:text-md md:text-xl bg-[#FEFEFE] rounded-xl w-1/5 h-1/2'>
+            RSVP
+            </button>
+           
+          </Link> 
+          
+          
+          </div>
+          </div>
+          
+
+         
+
+      </div>
+      <div className='grid grid-cols-5 divide-x bg-[#553ADD] rounded-3xl w-3/5 h-[18vw] pl-2 sm:pl-8 mt-10'>
+        <div className='flex-col text-[#EEEBFD] font-inter font-bold sm:col-span-1'>
+          <h1 className='flex text-xl sm:text-3xl md:text-6xl pt-3 md:pt-9 overflow-hidden'>9</h1>
+          <h2 className='flex text-xs sm:text-md md:text-xl overflow-hidden'>September</h2>
+          <h3 className='flex text-xs sm:text-sm md:text-lg overflow-hidden'>5:00PM</h3>
+
+          
+          </div>
+
+          <div className='grid col-span-4 text-[#EEEBFD] font-inter font-normal '>
+          <h1 className='flex-no-wrap text-md sm:text-3xl md:text-5xl pt-3 pl-5 md:pt-9 md:pl-6'>Rate my resume</h1>
+          <div className='grid justify-items-end mr-10 mt-6'>
+          <Link href="/" className='flex place-items-center'>
+            <button className='flex-text-center text-[#200263] font-bold text-xs sm:text-md md:text-xl bg-[#FEFEFE] rounded-xl w-1/5 h-1/2'>
+            RSVP
+            </button>
+           
+          </Link> 
+          
+          
+          </div>
+          </div>
+          
+
+         
+
+      </div>
+      
+      
+      <div className='flex justify-center mt-10 mb-10'>
+
+      <Link href="">
+                  <div className=" bg-[#553ADD] w-[25vw] text-white px-2 py-[1.2vw] text-center rounded-xl justify-self-center text-[2vw]">
+                    Go to Events Calender
+                  </div>
+                </Link>
+
+      </div>
+      
+  </div>
+
+
+  <h1 className='font-inter font-bold tracking-wider text-[#200263] text-center text-xl md:text-5xl pb-6 pt-12'>Industry professionals</h1>
+  <div className='grid grid-cols-3 mt-10 place-items-center '>
+  <div class="w-8/12 sm:w-6/12 place-items-center">
+    <img src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png" alt="..." class="shadow rounded-full max-w-full h-auto align-middle border-none" />
+    <h4 className='grid place-items-center font-inter text-[#200263] text-md pt-4'>Image1</h4>
+  </div>
+    <div class="w-8/12 sm:w-6/12 place-items-center">
+    <img src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png" alt="..." class="shadow rounded-full max-w-full h-auto align-middle border-none" />
+    <h4 className='grid place-items-center font-inter text-[#200263] text-md pt-4'>Image2</h4>
+  </div>
+    <div class="w-8/12 sm:w-6/12 place-items-center">
+    <img src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png" alt="..." class="shadow rounded-full max-w-full h-auto align-middle border-none" />
+    <h4 className='grid place-items-center font-inter text-[#200263] text-md pt-4 '>Image3</h4>
+  </div>
+
+
+
+  </div>
+  <div className='grid grid-cols-5 mt-10 place-items-center grid-flow-col auto-cols-fr '>
+    <div class="col-start-2 w-6/3 sm:w-10/12 place-items-end ">
+    <img src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png" alt="..." class="shadow rounded-full max-w-full h-auto align-middle border-none" />
+    <h4 className='grid place-items-center font-inter text-[#200263] text-md pt-4'>Image4</h4>
+  </div>
+    <div class="col-start-4 w-6/3 sm:w-10/12 place-items-center ">
+    <img src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png" alt="..." class="shadow rounded-full max-w-full h-auto align-middle border-none" />
+    <h4 className='grid place-items-center font-inter text-[#200263] text-md pt-4'>Image5</h4>
+  </div>    
+
+ 
+
+  </div>
+
+
+  </div>
+    
+    )
+}
+
+export default Prof_dev ;
