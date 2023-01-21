fbf00266cccf203abcf292ae99d8bbe462948923
David <53689532+RelativelyFine@users.noreply.github.com>
2023-01-21 07:37:05 -0500

    Directory Structure + Breaking Bugs

--- pages/prof_dev.jsx
+++ pages/prof_dev.jsx
@@ -1,19 +1,15 @@
-import Header from "../components/Header";
+import Header from "../components/Header/Header";
 import Navbar from "../components/Navbar";
-import Prof_dev from "../components/Prof_dev/P_d";
-import Footer from "../components/Footer";
-
+import Prof_dev from "../components/Prof_Dev/P_d";
+import Footer from "../components/FooterCollection/Footer";
 
 export default function Home() {
-    return (
-      <div>
-        <Header />
-        
-        <Prof_dev />
-        <Navbar color="light" />
-        <Footer />
-      </div>
-    );
-  }
-
-
+  return (
+    <div>
+      <Header />
+      <Prof_dev />
+      <Navbar color="light" />
+      <Footer />
+    </div>
+  );
+}
