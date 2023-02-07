8a974ecf560ca2a3cdcebcf732cfa9eb6d8282f3
David <53689532+RelativelyFine@users.noreply.github.com>
2023-02-07 00:52:11 -0500

    Recent

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: false,
+  reactStrictMode: true,
+  images: {
+    unoptimized: true,
+  },
 };
 
 module.exports = nextConfig;
